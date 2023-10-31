e6b810199d884318b7ee476969ced992d2704358
bariqahay <126383306+bariqahay@users.noreply.github.com>
2023-10-31 18:34:22 +0700

    Latihan vektor c++.cpp

--- Latihan vektor c++.cpp
+++ Latihan vektor c++.cpp
@@ -23,13 +23,13 @@ vector <vector<int>> vector_2d; //vector 2 dimensi
 vector_2d.push_back(vector1); //menambahkan elemen vector 1 ke vector2d
 vector_2d.push_back(vector2); //menambahkan elemen vector 2 ke vector2d
 
-cout << vector_2d.at(0).at(0) << endl; //menampilkan elemen vector2d pada 0,0
-cout << vector_2d.at(1).at(1) << endl; //menampilkan elemen vector2d pada 1,1
+cout << vector_2d.at(0).at(0) << vector_2d.at(0).at(1) << endl; //menampilkan elemen vector2d pada 0,0 dan 0,1
+cout << vector_2d.at(1).at(1) << vector_2d.at(1).at(0) << endl; //menampilkan elemen vector2d pada 1,1 dan 1,0
 /*Mengapa hanya sampai 0 dan 1? karena elemen pada vector 1 dan 2 hanya berjumlah 2, jadi masing" mempunyai index 0 dan 1.*/
 vector1.at(0) = 1000; //mengubah indeks 0 vektor 1 ke 1000
 /* disini hasil pada vector2d tidak terpengaruh karena vector2d hanya menampilkan yang sudah ditambahkan pada dia sebelumnya.*/
-cout << vector_2d.at(0).at(0) << endl; 
-cout << vector_2d.at(1).at(1) << endl;
+cout << vector_2d.at(0).at(0) << vector_2d.at(0).at(1) << endl; 
+cout << vector_2d.at(1).at(1) << vector_2d.at(1).at(0) << endl;
 cout << vector1.at(0) << endl; //menampilkan 1000
 cout << vector1.at(1) << endl; //menampilkan 20
 
